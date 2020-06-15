518ffecb3a9d963a2c2c4191b50e5c826e68ff7d
Omer Abarzel <omer.abarzel@live.biu.ac.il>
2020-06-15 22:22:17 +0300

    initial commit

---
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.flightmobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.text.set
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun connectButtonOnClick(view: View) {
+
+    }
+}
